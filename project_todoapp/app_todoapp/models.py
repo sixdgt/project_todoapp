c7e15d79c8997ffd170cb23bd44d08a323f0af28
sixdgt <c4crypt@gmail.com>
2022-07-25 08:57:53 +0545

    db setup & config, model creation, migration & migrate, template layouts formation

--- project_todoapp/app_todoapp/models.py
+++ project_todoapp/app_todoapp/models.py
@@ -1,3 +1,22 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_title = models.CharField(max_length=100)
+    task_desc = models.TextField(max_length=500)
+    task_category = models.CharField(max_length=100)
+    task_assign_date = models.DateField(default=0, null=True)
+    task_end_date = models.DateField(default=0, null=True)
+    assigned_by = models.CharField(max_length=200)
+    assigned_to = models.CharField(max_length=200)
+    is_removed = models.BooleanField(default=False, blank=True)
+    created_at = models.DateTimeField(default=datetime.now())
+    updated_at = models.DateTimeField(default=0, null=True, blank=True)
+    removed_at = models.DateTimeField(default=0, null=True, blank=True)
+
+    class Meta:
+        db_table = "tbl_task"
+
+    
+
--- project_todoapp/project_todoapp/settings.py
+++ project_todoapp/project_todoapp/settings.py
@@ -75,9 +75,18 @@ WSGI_APPLICATION = 'project_todoapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_todoapp',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
     }
 }
 
